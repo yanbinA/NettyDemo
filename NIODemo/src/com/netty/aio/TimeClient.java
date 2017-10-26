1449d389b05c07a11a57b81e599c23b76a235e72
yanbinA <yanbin7019@outlook.com>
2017-10-27 01:39:59 +0800

    AIO编程

---
+++ NIODemo/src/com/netty/aio/TimeClient.java
@@ -0,0 +1,8 @@
+package com.netty.aio;
+
+/**
+ * AIO客户端
+ * Created by yanbin on 2017/10/27 1:39
+ */
+public class TimeClient {
+}
---
+++ NIODemo/src/com/netty/aio/TimeServer.java
@@ -0,0 +1,8 @@
+package com.netty.aio;
+
+/**
+ * AIO服务端
+ * Created by yanbin on 2017/10/27 1:38
+ */
+public class TimeServer {
+}
