f215712bdc019c5d76bf89e0e3933c52bb6f74a9
yanbin <yanbin7019@outlook.com>
2017-10-26 14:02:33 +0800

    Netty入门案例

---
+++ parent/TimeServer/src/main/java/demo/netty/basic/TimeClient.java
@@ -0,0 +1,55 @@
+package demo.netty.basic;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.PrintWriter;
+
+/**
+ * @author yanbin
+ * @date 2017/10/26 11:54
+ */
+public class TimeClient {
+
+    public static void main(String[] args) throws Exception{
+        int port = 10001;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+
+    public void connect(int port, String host) throws Exception {
+        //配置客户端NIO线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+            //发起异步连接操作
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            //等待客户端链路关闭
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+}
---
+++ parent/TimeServer/src/main/java/demo/netty/basic/TimeClientHandler.java
@@ -0,0 +1,50 @@
+package demo.netty.basic;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelHandlerInvoker;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.util.logging.Logger;
+
+/**
+ * @author yanbin
+ * @date 2017/10/26 12:02
+ */
+public class TimeClientHandler extends ChannelHandlerAdapter {
+    private static final Logger LOGGER = Logger.getLogger(TimeClientHandler.class.getName());
+
+    private final ByteBuf firstMessage;
+
+    public TimeClientHandler() {
+        byte[] req = "QUERY TIME ORDER".getBytes();
+        firstMessage = Unpooled.buffer(req.length);
+        firstMessage.writeBytes(req);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        LOGGER.warning("Unexpected exception from downstream : " + cause.getMessage());
+        ctx.close();
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(firstMessage);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8");
+        System.out.println("Now is :" + body);
+
+
+    }
+}
